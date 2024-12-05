7c02b768bded38c9aa30591c7f6b6c49c7083fb7
aqsa <aqsa@gmail.com>
2024-12-05 19:23:11 +0500

    responsive carousel component review slider component

--- app/utils/fetchPagesData.js
+++ app/utils/fetchPagesData.js
@@ -1,8 +1,8 @@
 // initial api to get organization pages and its content
 export const fetchPagesData = async () => {
     const currentDomain = process.env.NEXT_PUBLIC_DOMAIN;
-    // https://8ip-resourceful-bohr.circumeo-apps.net/
-    const res = await fetch(`https://ffd1-59-103-120-85.ngrok-free.app/template/pages/?domain=${currentDomain}`);
+    // https://c8w-prudent-davy.circumeo-apps.net
+    const res = await fetch(`https://8ip-resourceful-bohr.circumeo-apps.net/template/pages/?domain=${currentDomain}`);
     const data = await res.json();
     // console.log("🚀 ~ fetchPagesData ~ data:", data)
     return data;
