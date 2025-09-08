842e5e8c28e15c0b45ea8e3cb6e4cb65757eee90
Aalian <aalianahmar123@gmail.com>
2025-09-08 13:13:37 +0500

    changes in simple footer

--- app/components/Footers/SimpleFooter/index.js
+++ app/components/Footers/SimpleFooter/index.js
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import React from "react";
 import { useTheme } from "../../../context/ThemeContext";
 import { useRouter } from "next/navigation";
 import { isAuthenticated } from '../../../utils/auth';
